8783ccf4e708cd828056b32d841ae4a38ebff206
Shakir Ayoub <53234349+shakirayoub56@users.noreply.github.com>
2022-05-23 23:36:45 -0700

    final
    final

---
+++ my-app/src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+ return (
+   <>
+   <nav>
+     <h1>Mobile Operating System</h1>
+     <li>Andriod</li>
+     <li>Blackberry</li>
+     <li>Iphone</li>
+     <li>Windows Phone</li>
+   </nav>
+   <nav>
+     <h1>Mobile Manifacturers</h1>
+     <li>Samsung</li>
+     <li>HTC</li>
+     <li>Micromax</li>
+     <li>Apple</li>
+   </nav>
+   </>
+
+ 
+  );
+}
+
+export default App;
